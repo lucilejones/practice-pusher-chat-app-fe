45645979d80c5bfac3b9f907746f9038906f5fac
Lucile Jones <jones.lucile@gmail.com>
2024-03-12 16:04:50 -0600

    setup basic chat app with pusher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from './message.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fe-pusher-chat-app';
+
+export class AppComponent implements OnInit {
+  messages: any[] = [];
+  newMessage: string = '';
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.messageService.getMessages().subscribe((messages: any) => {
+      this.messages = messages;
+    });
+
+    this.messageService.subscribeToNewMessages((message: any) => {
+      this.messages.push(message);
+    });
+  }
+
+  // sendMessage(): void {
+  //   this.messageService.sendMessage(this.newMessage).subscribe((message: any) => {
+  //     this.messages.push(message);
+  //     this.newMessage = '';
+  //   });
+  // }
+  sendMessage(): void {
+    this.messageService.sendMessage(this.newMessage).subscribe((message: any) => {
+      if (!this.messages.find((m: any) => m.id === message.id)) {
+        this.messages.push(message);
+      }
+      this.newMessage = '';
+    });
+  }
 }
